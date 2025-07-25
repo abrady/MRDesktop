6b9bff53a41830ed8a7ff1d832739d83f2beb6a0
Aaron Brady <abrady@meta.com>
2025-07-24 20:28:02 -0700

    unreal project loading and material properly set

---
+++ src/clients/unreal/Source/PTSamples/PTSamples.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PTSamples.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PTSamples, "PTSamples" );
